48127be8ce69359f8d0b586121a2897e63bb02c5
Enrique Shunnar <xtealer00@gmail.com>
2023-01-31 22:58:55 -0500

    feat/v1: Update font style and icons.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import "@/styles/globals.css";
 
 import { AppPropsWithLayout } from "@/types/Layout";
 import AppProviders from "@/AppProviders";
-import { Inter } from "@next/font/google";
+import { Poppins } from "@next/font/google";
 import Head from "next/head";
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "500",
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
@@ -20,9 +24,9 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
       </Head>
-      <style jsx global className={inter.variable}>{`
+      <style jsx global className={poppins.variable}>{`
         html {
-          font-family: ${inter.style.fontFamily} !important;
+          font-family: ${poppins.style.fontFamily} !important;
         }
       `}</style>
       {getLayout(<Component {...pageProps} />)}
