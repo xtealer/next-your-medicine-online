aa4a0547f5187d3fbb26a0d1b57a99d32122579f
Enrique Shunnar <xtealer00@gmail.com>
2023-02-16 13:38:11 -0500

    feat/v1.3: Improve products ui responsive.

--- src/components/products/OzempicProduct.tsx
+++ src/components/products/OzempicProduct.tsx
@@ -77,91 +77,85 @@ const OzempicProduct: FC = () => {
           </BreadcrumbItem>
         </Breadcrumb>
       )}
-
-      <Flex
-        flexDirection={{ base: "column-reverse", md: "row" }}
-        justifyContent="center"
-        pt="12"
-      >
+      <Flex flexDirection="column" flex="1" px={{ base: "0", md: "6" }} pt="12">
         <Flex
-          flexDirection="column"
-          flex="1"
-          maxW="600px"
-          px={{ base: "0", md: "6" }}
+          flexDirection={{ base: "column", md: "row" }}
+          justifyContent="center"
+          align="center"
+          gap="10"
         >
           <ImagesPreview images={images} />
-          <Text fontWeight="semibold" pt="6">
-            Ozempic® (semaglutide) injection 0.5 mg, 1 mg, or 2 mg is an
-            injectable prescription medication used:
-          </Text>
-
-          <Text fontWeight="regular" pt="6">
-            Along with diet and exercise to improve blood sugar (glucose) levels
-            in adults with type 2 diabetes mellitus, to reduce the risk of
-            serious cardiovascular events such as heart attack, stroke or death
-            in adults with type 2 diabetes mellitus with confirmed heart
-            disease.
-            <br />
-            <br />
-            It is not known whether Ozempic® can be used in people who have had
-            pancreatitis.
-            <br />
-            Ozempic® is not for use in people with type 1 diabetes.
-            <br />
-            <br />
-            Ozempic® is a medicine for adults with type 2 diabetes that, along
-            with diet and exercise, may improve blood sugar. While not for
-            weight loss, Ozempic® may help you lose some weight.
-          </Text>
-
-          <Text pt="6" textColor="primary.500" fontWeight="semibold">
-            Please consult your general doctor for better information.
-          </Text>
-        </Flex>
-        <Flex
-          px={{ base: "0", md: "6" }}
-          flexDirection="column"
-          flex="1"
-          pb="12"
-          maxW="600px"
-          alignItems={{ base: "center", md: "unset" }}
-        >
-          <Heading
-            as="h1"
-            fontWeight="bold"
-            textAlign={{ base: "center", md: "left" }}
+          <Flex
+            px={{ base: "0", md: "6" }}
+            flexDirection="column"
+            flex="1"
+            pb="12"
+            maxW="600px"
+            alignItems={{ base: "center", md: "unset" }}
           >
-            Ozempic Pen 2mg / 1,5ml
-          </Heading>
-
-          <Text fontWeight="semibold" pt="2" pb="3" fontSize="2xl">
-            Price: $600
-          </Text>
-
-          <PayPalButton />
-
-          <Flex pt="6" gap="3">
-            <WhatsappButton link="https://api.whatsapp.com/send?text=https://yourmedicine.online/products/ozempic" />
-            <FacebookButton link="https://www.facebook.com/sharer/sharer.php?u=https://www.yourmedicine.online/products/ozempic" />
-            <TwitterButton link="https://twitter.com/intent/tweet?url=https://www.yourmedicine.online/products/ozempic&text=Ozempic" />
-          </Flex>
-
-          <Text fontWeight="semibold" pt="6" w="full">
-            Shipping to the United States cost is $150.00 (all couriers) **
-            <br />
-            <br />
-            Remember to send us a copy of your PayPal Receipt to{" "}
-            <ChakraLink
-              color="primary._"
-              textDecoration="underline"
-              href="mailto:yourmedicineonlinee@gmail.com"
-              isExternal
+            <Heading
+              as="h1"
+              fontWeight="bold"
+              textAlign={{ base: "center", md: "left" }}
             >
-              yourmedicineonlinee@gmail.com
-            </ChakraLink>{" "}
-            for order confirmation.
-          </Text>
+              Ozempic Pen 2mg / 1,5ml
+            </Heading>
+
+            <Text fontWeight="semibold" pt="2" pb="3" fontSize="2xl">
+              Price: $600
+            </Text>
+
+            <PayPalButton />
+
+            <Flex pt="6" gap="3">
+              <WhatsappButton link="https://api.whatsapp.com/send?text=https://yourmedicine.online/products/ozempic" />
+              <FacebookButton link="https://www.facebook.com/sharer/sharer.php?u=https://www.yourmedicine.online/products/ozempic" />
+              <TwitterButton link="https://twitter.com/intent/tweet?url=https://www.yourmedicine.online/products/ozempic&text=Ozempic" />
+            </Flex>
+
+            <Text fontWeight="semibold" pt="6" w="full">
+              Shipping to the United States cost is $150.00 (all couriers) **
+              <br />
+              <br />
+              Remember to send us a copy of your PayPal Receipt to{" "}
+              <ChakraLink
+                color="primary._"
+                textDecoration="underline"
+                href="mailto:yourmedicineonlinee@gmail.com"
+                isExternal
+              >
+                yourmedicineonlinee@gmail.com
+              </ChakraLink>{" "}
+              for order confirmation.
+            </Text>
+          </Flex>
         </Flex>
+        <Text fontWeight="semibold" pt="6">
+          Ozempic® (semaglutide) injection 0.5 mg, 1 mg, or 2 mg is an
+          injectable prescription medication used:
+        </Text>
+
+        <Text fontWeight="regular" pt="6">
+          Along with diet and exercise to improve blood sugar (glucose) levels
+          in adults with type 2 diabetes mellitus, to reduce the risk of serious
+          cardiovascular events such as heart attack, stroke or death in adults
+          with type 2 diabetes mellitus with confirmed heart disease.
+          <br />
+          <br />
+          It is not known whether Ozempic® can be used in people who have had
+          pancreatitis.
+          <br />
+          Ozempic® is not for use in people with type 1 diabetes.
+          <br />
+          <br />
+          Ozempic® is a medicine for adults with type 2 diabetes that, along
+          with diet and exercise, may improve blood sugar. While not for weight
+          loss, Ozempic® may help you lose some weight.
+        </Text>
+
+        <Text pt="6" textColor="primary.500" fontWeight="semibold">
+          Please consult your general doctor for better information.
+        </Text>
       </Flex>
     </Flex>
   );
